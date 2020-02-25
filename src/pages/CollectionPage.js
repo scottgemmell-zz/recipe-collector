1e3709071811785797bbda2a594ab9c25d2495de
Scott Gemmell <scott.gemmell@mac.com>
2020-02-25 21:30:16 +0000

    fetchFavourites using promise all

--- src/pages/CollectionPage.js
+++ src/pages/CollectionPage.js
@@ -5,21 +5,21 @@ const CollectionPage = () => {
 
 	const [recipeResource, setRecipeResource] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
+	const [initialState] = useState([52772, 52773, 52774]);
 
 	useEffect(() => {
-		fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772")
-		// data
-		//fetch("https://api.spoonacular.com/recipes/informationBulk?ids=716367,715568&apiKey=91b3e1d90c3745ed8f818cef3f0f4070")
+		//console.log("STARTUP");
+		const fetchFavourites = initialState.map(mealId => {
+			return fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
 			.then(res => res.json())
-			.then(data => {
-				setRecipeResource(data.meals);
-				setIsLoading(false);
-			})
-			.catch(err => console.log(err));
-		// return () => {
-		// 	cleanup
-		// };
-	}, []);
+			.then(data => data.meals[0])
+		});
+		Promise.all(fetchFavourites)
+		.then(data => {
+			setRecipeResource(data);
+			setIsLoading(false)
+		})
+	}, [initialState]);
 
   return (
     <div className="App-main">
