0a9530bc725b8c7d7a6bab8d0d686385981453c2
Scott Gemmell <scott.gemmell@mac.com>
2020-03-04 11:16:15 +0000

    styling

--- src/common/RecipesThumb.js
+++ src/common/RecipesThumb.js
@@ -10,14 +10,19 @@ function RecipesItem({ idMeal, isFav, strMealThumb, strMeal, handleFavourite }) 
 			<img 
 				className="thumb__img" 
 				src={strMealThumb} 
-				// width={140} 
-				// height={90} 
+				width={240} 
+				height={240} 
 				alt={strMeal} 
 			/> 
 			<span className="thumb__meta">
-				<Link className="thumb__title" to={`/item/${idMeal}/`}>{strMeal}</Link> 
-				<button className="thumb__add" onClick={() => handleFavourite({ idMeal })}>
-  					{isFav ? <FaHeart /> : <FaRegHeart />} <span className="u-visually-hidden">Add</span>
+				<Link className="thumb__title" to={`/item/${idMeal}/`}>
+					{strMeal}
+				</Link> 
+				<button className="thumb__toggle" onClick={() => handleFavourite({ idMeal })}>
+  					{isFav ? <FaHeart /> : <FaRegHeart />} 
+					  <span className="u-visually-hidden">
+					  {isFav ? "Add to" : "Remove from"} favourites
+					  </span>
 				</button> 
 			</span>
 		</li>
