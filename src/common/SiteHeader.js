32f45b868773555c99b145502cf0c34afe197a05
Scott Gemmell <scott.gemmell@mac.com>
2020-03-04 18:11:00 +0000

    Total

--- src/common/SiteHeader.js
+++ src/common/SiteHeader.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectFavourites } from "../features/favourites/favouritesSlice";
+import { FaHeart } from 'react-icons/fa';
 import logo from '../logo.svg';
 
 export default function SiteHeader() {
+	const myFavourites = useSelector(selectFavourites);
 	return (
 		<header className="App-header">
 			<div className="App-inner">
@@ -14,9 +18,15 @@ export default function SiteHeader() {
 				</div>
 				<nav className="App-nav">
 					<ul>
-						<li><Link to="/list/">List</Link></li>
-						<li><Link to="/favourites/">Favourites</Link></li>
-						<li><Link to="/example/">Example</Link></li>
+						<li>
+							<Link to="/list/">All recipes</Link>
+						</li>
+						<li>
+							<Link className="total" to="/favourites/"><FaHeart /></Link> <span>{myFavourites.length}</span>
+						</li>
+						{/* <li>
+							<Link to="/example/">Example</Link>
+						</li> */}
 					</ul>
 				</nav>
 			</div>
