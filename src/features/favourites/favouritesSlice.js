c56950bbc117b6b35d70c99c42c7e15e4c645f73
Scott Gemmell <scott.gemmell@mac.com>
2020-03-09 21:14:34 +0000

    localStorage

--- src/features/favourites/favouritesSlice.js
+++ src/features/favourites/favouritesSlice.js
@@ -3,13 +3,17 @@ import { createSlice } from "@reduxjs/toolkit";
 export const slice = createSlice({
   name: 'favourites',
   initialState: {
-    value: [52776, 52855, 52894],
+    value: JSON.parse(window.localStorage.getItem("favourites")),
   },
   reducers: {
     toggleFavourite: (state, action) => {
 		//console.log({ state, action, value: +action.payload });
 		const hasFavourite = state.value.indexOf(+action.payload);
-    	hasFavourite === -1 ? state.value.push(+action.payload) : state.value.splice(hasFavourite, 1);
+		hasFavourite === -1 
+			? state.value.push(+action.payload) 
+			: state.value.splice(hasFavourite, 1);
+		window.localStorage.setItem("favourites", JSON.stringify(state.value));
+		
 	},
   },
 });
--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -11,6 +11,10 @@ const ListPage = () => {
 	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
+		window.localStorage.setItem("favourites", JSON.stringify(myFavourites));
+	}, [myFavourites]);
+
+	useEffect(() => {
 		fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=b")
 		// data.recipes
 		//fetch("https://api.spoonacular.com/recipes/random?number=20&tags=vegetarian&apiKey=3fcd89c737204c9d9e588415088e7b96")
