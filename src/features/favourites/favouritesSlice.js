1599f2a7ed5bf74c62ed64e60a643181480773fd
Scott Gemmell <scott.gemmell@mac.com>
2020-03-09 21:36:49 +0000

    indent

--- src/features/favourites/favouritesSlice.js
+++ src/features/favourites/favouritesSlice.js
@@ -1,20 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const slice = createSlice({
-  name: 'favourites',
-  initialState: {
-    value: JSON.parse(window.localStorage.getItem("favourites")),
-  },
-  reducers: {
-    toggleFavourite: (state, action) => {
-		//console.log({ state, action, value: +action.payload });
-		const hasFavourite = state.value.indexOf(+action.payload);
-		hasFavourite === -1 
-			? state.value.push(+action.payload) 
-			: state.value.splice(hasFavourite, 1);
-		window.localStorage.setItem("favourites", JSON.stringify(state.value));
+	name: "favourites",
+	initialState: {
+    	value: JSON.parse(window.localStorage.getItem("favourites")),
 	},
-  },
+	reducers: {
+    	toggleFavourite: (state, action) => {
+			//console.log({ state, action, value: +action.payload });
+			const hasFavourite = state.value.indexOf(+action.payload);
+			hasFavourite === -1 
+				? state.value.push(+action.payload) 
+				: state.value.splice(hasFavourite, 1);
+			window.localStorage.setItem("favourites", JSON.stringify(state.value));
+		},
+	},
 });
 
 export const selectFavourites = state => state.favourites.value;
